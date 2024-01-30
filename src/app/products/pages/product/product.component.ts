0ced2c670240fa8f83887b17c9f44be936fb61f3
Jonathan Pillajo <pandc19@outlook.com>
2024-01-29 22:42:58 -0500

    fin sección 20

---
+++ src/app/products/pages/product/product.component.ts
@@ -0,0 +1,43 @@
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'products-product=page',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+
+  constructor() {
+    console.log('Contructor');
+  }
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('ngOnChanges');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  increasePrice() {
+    this.currentPrice++;
+  }
+
+}
